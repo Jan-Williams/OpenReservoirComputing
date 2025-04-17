503083e7f48ab1e1b436751a94545881a45316ed
Jan-Williams <jmpw1@uw.edu>
2025-04-17 14:03:45 -0700

    Updated reservoir drivers to allow for easy parallelization

--- tests/test_drivers.py
+++ tests/test_drivers.py
@@ -1,6 +1,7 @@
 import jax
 import jax.numpy as jnp
 import pytest
+from jax.experimental import sparse
 
 import orc
 
@@ -21,11 +22,14 @@ def esndriver():
 def test_esndriver_dims(esndriver):
     key = jax.random.key(999)
     res_dim = esndriver.res_dim
-    test_vec = jax.random.normal(key, shape=(res_dim))
+    test_vec = jax.random.normal(key, shape=(1, res_dim))
     out_vec = esndriver.advance(test_vec, test_vec)
-    assert out_vec.shape == (res_dim,)
+    assert out_vec.shape == (
+        1,
+        res_dim,
+    )
 
-    test_vec = jax.random.normal(key, shape=(res_dim - 1))
+    test_vec = jax.random.normal(key, shape=(1, res_dim - 1))
     with pytest.raises(ValueError):
         out_vec = esndriver.advance(test_vec, test_vec)
 
@@ -34,12 +38,12 @@ def test_esndriver_dims(esndriver):
 def test_batchapply_dims_esn(batch_size, esndriver):
     key = jax.random.key(42)
     res_dim = esndriver.res_dim
-    test_vec = jax.random.normal(key, shape=(batch_size, res_dim))
+    test_vec = jax.random.normal(key, shape=(batch_size, 1, res_dim))
     out_vec = esndriver.batch_advance(test_vec, test_vec)
 
-    assert out_vec.shape == (batch_size, res_dim)
+    assert out_vec.shape == (batch_size, 1, res_dim)
 
-    test_vec = jax.random.normal(key, shape=(batch_size, res_dim - 1))
+    test_vec = jax.random.normal(key, shape=(batch_size, 1, res_dim - 1))
 
     with pytest.raises(ValueError):
         out_vec = esndriver.batch_advance(test_vec, test_vec)
@@ -52,7 +56,7 @@ def test_batchapply_dims_esn(batch_size, esndriver):
         (22.2, 0.123, 0.6, 0.02, 1.6, jnp.float64),
     ],
 )
-def test_param_types_linearreadout(res_dim, leak, spec_rad, density, bias, dtype):
+def test_param_types_esn(res_dim, leak, spec_rad, density, bias, dtype):
     with pytest.raises(TypeError):
         _ = orc.drivers.ESNDriver(
             res_dim=res_dim,
@@ -73,7 +77,7 @@ def test_param_types_linearreadout(res_dim, leak, spec_rad, density, bias, dtype
         (22, -0.2, 0.6, 0.04, 1.6, jnp.float32),
     ],
 )
-def test_param_vals_linearreadout(res_dim, leak, spec_rad, density, bias, dtype):
+def test_param_vals_esn(res_dim, leak, spec_rad, density, bias, dtype):
     with pytest.raises(ValueError):
         _ = orc.drivers.ESNDriver(
             res_dim=res_dim,
@@ -84,3 +88,39 @@ def test_param_vals_linearreadout(res_dim, leak, spec_rad, density, bias, dtype)
             dtype=dtype,
             seed=32,
         )
+
+
+@pytest.mark.parametrize("groups", [2, 4, 8, 9])
+def test_call_ones_esn(groups):
+    model = orc.drivers.ESNDriver(
+        res_dim=212,
+        leak=0.123,
+        spec_rad=0.6,
+        density=0.02,
+        bias=1.6,
+        dtype=jnp.float64,
+        seed=0,
+        groups=groups,
+    )
+    key = jax.random.key(0)
+    key1, key2 = jax.random.split(key)
+    test_vec1 = jax.random.normal(key=key1, shape=(groups, 212))
+    test_vec2 = jax.random.normal(key=key2, shape=(groups, 212))
+    test_outputs = model(test_vec1, test_vec2)
+    wr = sparse.BCOO.todense(model.wr)
+    bias = model.bias
+    leak = model.leak
+
+    def naive_imp_forward(wr, bias, leak, proj_vars, res_state):
+        res_next = jnp.tanh(
+            wr @ res_state + proj_vars + bias * jnp.ones_like(proj_vars)
+        )
+        return leak * res_next + (1 - leak) * res_state
+
+    gt_outputs = jnp.empty((groups, 212))
+    for group in range(groups):
+        gt = naive_imp_forward(
+            wr[group], bias, leak, test_vec1[group], test_vec2[group]
+        )
+        gt_outputs = gt_outputs.at[group].set(gt)
+    assert jnp.allclose(gt_outputs, test_outputs)
