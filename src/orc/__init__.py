0442a3f961f1f57fc9f1da7fd417d593c82152a8
dtretiak <dstretiak@gmail.com>
2025-05-01 01:47:49 -0700

    updated file locations to make importing of integrator functions to make more sense

--- src/orc/__init__.py
+++ src/orc/__init__.py
@@ -1,5 +1,5 @@
 """Components of models."""
 
-from orc import drivers, embeddings, models, rc, readouts, utils
+from orc import drivers, embeddings, models, rc, readouts, utils, data
 
-__all__ = ["drivers", "embeddings", "models", "readouts", "utils", "rc"]
+__all__ = ["drivers", "embeddings", "models", "readouts", "utils", "rc", "data"]
---
+++ src/orc/data/__init__.py
@@ -0,0 +1,6 @@
+"""Implementations of data generation and handling functions."""
+
+from orc.data import integrators
+from orc.data.integrators import *
+
+__all__ = ["integrators"]
