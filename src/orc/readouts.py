c13b02ede05f5ce80c2ac2c7fba049d416fb1d20
Jan-Williams <jmpw1@uw.edu>
2025-10-16 14:38:53 -0700

    Added single readout layers

--- src/orc/readouts.py
+++ src/orc/readouts.py
@@ -211,6 +211,91 @@ class ParallelLinearReadout(ReadoutBase):
         return to_ret
 
 
+class LinearReadout(ParallelLinearReadout):
+    """Linear readout layer.
+
+    Attributes
+    ----------
+    out_dim : int
+        Dimension of reservoir output.
+    res_dim : int
+        Reservoir dimension.
+    wout : Array
+        Output matrix.
+    dtype : Float
+            Dtype, default jnp.float64.
+
+    Methods
+    -------
+    readout(res_state)
+        Map from reservoir state to output state.
+    """
+
+    def __init__(
+        self,
+        out_dim: int,
+        res_dim: int,
+        dtype: Float = jnp.float64,
+        *,
+        seed: int = 0,
+    ) -> None:
+        """Initialize readout layer to zeros.
+
+        Parameters
+        ----------
+        out_dim : int
+            Dimension of reservoir output.
+        res_dim : int
+            Reservoir dimension.
+        chunks : int
+            Number of parallel resrevoirs.
+        dtype : Float
+            Dtype, default jnp.float64.
+        seed : int
+            Not used for ParallelLinearReadout, here to maintain consistent
+            interface.
+        """
+        super().__init__(
+        out_dim=out_dim,
+        res_dim=res_dim,
+        chunks=1,
+        dtype=dtype,
+        seed=seed,
+        )
+
+    @eqx.filter_jit
+    def readout(self, res_state: Array) -> Array:
+        """Readout from reservoir state.
+
+        Parameters
+        ----------
+        res_state : Array
+            Reservoir state, (shape=(res_dim,)).
+
+        Returns
+        -------
+        Array
+            Output from reservoir, (shape=(out_dim,)).
+        """
+        return super().readout(res_state.reshape(1, -1)).reshape(-1)
+
+    def __call__(self, res_state: Array) -> Array:
+        """Call either readout or batch_readout depending on dimensions.
+
+        Parameters
+        ----------
+        res_state : Array
+            Reservoir state, (shape=(res_dim) or
+            shape=(seq_len, res_dim)).
+
+        Returns
+        -------
+        Array
+            Output state, (out_dim,) or shape=(seq_len, out_dim)).
+        """
+        return jnp.squeeze(super().__call__(res_state[..., None, :]))
+
+
 class ParallelNonlinearReadout(ReadoutBase):
     """Readout layer with user specified nonlinearities.
 
@@ -360,6 +445,133 @@ class ParallelNonlinearReadout(ReadoutBase):
         return to_ret
 
 
+class NonlinearReadout(ParallelNonlinearReadout):
+    """Readout layer with user specified nonlinearities.
+
+    Attributes
+    ----------
+    out_dim : int
+        Dimension of reservoir output.
+    res_dim : int
+        Reservoir dimension.
+    chunks : int
+        Number of parallel reservoirs.
+    wout : Array
+        Output matrix.
+    nonlin_list : list
+        List containing user specified nonlinearities.
+    dtype : Float
+            Dtype, default jnp.float64.
+
+    Methods
+    -------
+    nonlinear_transform(res_state)
+        Nonlinear transform that acts entrywise on reservoir state.
+    readout(res_state)
+        Map from reservoir state to output state.
+    __call__(res_state)
+        Map from reservoir state to output state, handles batch and single outputs.
+    """
+
+    def __init__(
+        self,
+        out_dim: int,
+        res_dim: int,
+        nonlin_list: list[Callable],
+        dtype: Float = jnp.float64,
+        *,
+        seed: int = 0,
+    ) -> None:
+        """Initialize readout layer to zeros.
+
+        Parameters
+        ----------
+        out_dim : int
+            Dimension of reservoir output.
+        res_dim : int
+            Reservoir dimension.
+        nonlin_list : list[Callable]
+            List containing user specified entrywise nonlinearities. Each entry should
+            be a function mapping a scalar value to another scalar value, e.g.
+            lambda x : x ** 2 or lambda x : jnp.sin(x).
+        dtype : Float
+            Dtype, default jnp.float64.
+        seed : int
+            Not used for ParallelNonlinearReadout, here to maintain consistent
+            interface.
+        """
+        super().__init__(
+            out_dim=out_dim,
+            res_dim=res_dim,
+            nonlin_list=nonlin_list,
+            chunks=1,
+            dtype=dtype,
+            seed=seed,
+        )
+
+    def nonlinear_transform(self, res_state: Array) -> Array:
+        """Perform nonlinear transformation on reservoir state.
+
+        Let tot_list be the list consisting of nonlin_list prepended by the identity
+        mapping. Let n be the length of tot_list. Then, nonlinear_transform acts such
+        that for all 0 <= k < chunks and 0 <= j < j * n:
+        res_state[k, j * n] <- res_state[k, j*n]
+        res_state[k, j * n + 1] <- f_0(res_state[k, j * n + 1])
+        ...
+        res_state[k, j * n + n - 1] <- f_{n-1}(res_state[k, j * n + n - 1])
+        where f_i is the i-th entry of nonlin_list.
+
+        Parameters
+        ----------
+        res_state : Array
+            Reservoir state, (shape=(chunks, res_dim,)).
+
+        Returns
+        -------
+        Array
+            Transformed reservoir state.
+        """
+        num_nonlins = len(self.nonlin_list)
+        for idx in range(num_nonlins):
+            transformed_res_state = res_state.at[:, idx + 1 :: num_nonlins + 1].set(
+                self.nonlin_list[idx](res_state[:, idx + 1 :: num_nonlins + 1])
+            )
+        return transformed_res_state
+
+    @eqx.filter_jit
+    def readout(self, res_state: Array) -> Array:
+        """Readout from reservoir state.
+
+        Parameters
+        ----------
+        res_state : Array
+            Reservoir state, (shape=(res_dim,)).
+
+        Returns
+        -------
+        Array
+            Output from reservoir, (shape=(out_dim,)).
+        """
+        return super().readout(res_state.reshape(1, -1)).reshape(-1)
+
+    def __call__(self, res_state: Array) -> Array:
+        """Call either readout or batch_readout depending on dimensions.
+
+        Parameters
+        ----------
+        res_state : Array
+            Reservoir state, (shape=(res_dim) or
+            shape=(seq_len, res_dim)).
+
+        Returns
+        -------
+        Array
+            Output state, (out_dim,) or shape=(seq_len, out_dim)).
+        """
+        return jnp.squeeze(super().__call__(res_state[..., None, :]))
+
+
+
 class ParallelQuadraticReadout(ParallelNonlinearReadout):
     """Quadratic readout layer.
 
@@ -424,4 +636,62 @@ class ParallelQuadraticReadout(ParallelNonlinearReadout):
             dtype=dtype,
             nonlin_list=[lambda x: x**2],
             chunks=chunks,
+        )
+
+
+class QuadraticReadout(NonlinearReadout):
+    """Quadratic readout layer.
+
+    Attributes
+    ----------
+    out_dim : int
+        Dimension of reservoir output.
+    res_dim : int
+        Reservoir dimension.
+    wout : Array
+        Output matrix.
+    dtype : Float
+            Dtype, default jnp.float64.
+
+    Methods
+    -------
+    nonlinear_transform(res_state)
+        Quadratic transform that acts entrywise on reservoir state.
+    readout(res_state)
+        Map from reservoir state to output state with quadratic nonlinearity.
+    __call__(res_state)
+        Map from reservoir state to output state with quadratic nonlinearity,
+        handles batch and single outputs.
+    """
+
+    def __init__(
+        self,
+        out_dim: int,
+        res_dim: int,
+        chunks: int = 1,
+        dtype: Float = jnp.float64,
+        *,
+        seed: int = 0,
+    ) -> None:
+        """Initialize readout layer to zeros.
+
+        Parameters
+        ----------
+        out_dim : int
+            Dimension of reservoir output.
+        res_dim : int
+            Reservoir dimension.
+        chunks : int
+            Number of parallel resrevoirs.
+        dtype : Float
+            Dtype, default jnp.float64.
+        seed : int
+            Not used for ParallelQuadraticReadout, here to maintain consistent
+            interface.
+        """
+        super().__init__(
+            out_dim=out_dim,
+            res_dim=res_dim,
+            dtype=dtype,
+            nonlin_list=[lambda x: x**2],
         )
